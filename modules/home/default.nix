dcc06d993009d5b4e7a480fad605be971fa09aca
Pascal Diehm <pdiehm8@gmail.com>
2024-10-03 23:47:45 +0200

    Added more packages without configurations

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,1 +1,1 @@
-{ ... }: { imports = [ ./chrome.nix ./git.nix ./home-manager.nix ./home.nix ./kde.nix ./plasma.nix ./vim.nix ./zsh.nix ]; }
+{ ... }: { imports = [ ./git.nix ./home-manager.nix ./home.nix ./kde.nix ./plasma.nix ./programs.nix ./vim.nix ./zsh.nix ]; }
