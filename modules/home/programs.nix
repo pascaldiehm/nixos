07b1a4e4922c9bce6b58026081abde2d9ae313d9
Pascal Diehm <pdiehm8@gmail.com>
2024-10-03 23:47:45 +0200

    Added more packages without configurations

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }: {
-  # Install google-chrome
-  home.packages = [ pkgs.google-chrome ];
+  # Programs
+  home.packages = [
+    pkgs.bitwarden-desktop
+    pkgs.google-chrome
+    pkgs.vlc
+  ];
 
-  # Set as default browser
+  # Mime types
   xdg.mimeApps.defaultApplications = {
     "text/html" = "google-chrome.desktop";
     "x-scheme-handler/http" = "google-chrome.desktop";
