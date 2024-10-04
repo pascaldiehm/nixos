08b3ecd02169c1bc99225cf61f74824a8b944999
Pascal Diehm <pdiehm8@gmail.com>
2024-10-04 08:16:50 +0200

    Use multiple lines in module group imports

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,1 +1,12 @@
-{ ... }: { imports = [ ./git.nix ./home-manager.nix ./home.nix ./kde.nix ./plasma.nix ./programs.nix ./vim.nix ./zsh.nix ]; }
+{ ... }: {
+  imports = [
+    ./git.nix
+    ./home-manager.nix
+    ./home.nix
+    ./kde.nix
+    ./plasma.nix
+    ./programs.nix
+    ./vim.nix
+    ./zsh.nix
+  ];
+}
