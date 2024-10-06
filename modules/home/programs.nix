01829ccf457ceac40b4fc051b0c096583c109dff
Pascal Diehm <pdiehm8@gmail.com>
2024-10-06 12:42:06 +0200

    Switched browser to Firefox

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -2,14 +2,6 @@
   # Programs
   home.packages = [
     pkgs.bitwarden-desktop
-    pkgs.google-chrome
     pkgs.vlc
   ];
-
-  # Mime types
-  xdg.mimeApps.defaultApplications = {
-    "text/html" = "google-chrome.desktop";
-    "x-scheme-handler/http" = "google-chrome.desktop";
-    "x-scheme-handler/https" = "google-chrome.desktop";
-  };
 }
