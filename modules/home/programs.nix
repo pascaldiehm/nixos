0b32c98fcacd77a2acb1b9bb528d2828ee20a930
Pascal Diehm <pdiehm8@gmail.com>
2024-10-16 15:42:07 +0200

    Added php

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -7,6 +7,7 @@
     pkgs.kdePackages.filelight
     pkgs.kdePackages.kdeconnect-kde
     pkgs.kdePackages.partitionmanager
+    pkgs.php
     pkgs.python3
     pkgs.texlive.combined.scheme-full
     pkgs.vlc
