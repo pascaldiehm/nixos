8abaa2ceb03c656aaac1781e25e292892cd706a5
Pascal Diehm <pdiehm8@gmail.com>
2024-10-16 14:34:56 +0200

    Added global yarn packages

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -7,10 +7,8 @@
     pkgs.kdePackages.filelight
     pkgs.kdePackages.kdeconnect-kde
     pkgs.kdePackages.partitionmanager
-    pkgs.nodejs
     pkgs.texlive.combined.scheme-full
     pkgs.vlc
-    pkgs.yarn
     pkgs.yt-dlp
   ];
 }
