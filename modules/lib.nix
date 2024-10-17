a012a2ada6bd3799c2cba95ce9bacac2697da27a
Pascal Diehm <pdiehm8@gmail.com>
2024-10-17 09:42:45 +0200

    Added custom library

---
+++ modules/lib.nix
@@ -0,0 +1,15 @@
+{
+  mkFirefoxBookmarks = set: builtins.map (name: { name = name; url = set.${name}; }) (builtins.attrNames set);
+
+  mkFirefoxExtensions = list: builtins.listToAttrs (builtins.map (id: {
+    name = id;
+    value = {
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+    };
+  }) list);
+
+  mkPkgList = pkgs: builtins.concatStringsSep "\n" (builtins.sort (a: b: a < b) (builtins.map (p: p.name) pkgs));
+
+  secret = key: (builtins.fromJSON (builtins.readFile /etc/nixos/secrets.json)).${key};
+}
