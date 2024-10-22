eea98bd86aca4eadf1f0c44662aaafaf2554689f
Pascal Diehm <pdiehm8@gmail.com>
2024-10-22 06:34:39 +0200

    Converted auto-mute to systemd unit

--- machines/pascal-pc.nix
+++ machines/pascal-pc.nix
@@ -17,7 +17,12 @@
   };
 
   # Disable auto-mute
-  home-manager.users.pascal.home.activation.disableAutoMute = helpers.mkHomeManagerActivation [ "writeBoundary" ] "run ${pkgs.alsa-utils}/bin/amixer -c 2 sset 'Auto-Mute Mode' Disabled";
+  systemd.services.disableAutoMute = {
+    after = [ "sound.target" ];
+    description = "Disable auto-mute";
+    script = "${pkgs.alsa-utils}/bin/amixer -c 2 sset 'Auto-Mute Mode' Disabled";
+    wantedBy = [ "multi-user.target" ];
+  };
 
   # Setup printer
   hardware.sane.enable = true;
