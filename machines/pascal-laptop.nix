ca1d525cf781c0a134b0fcc96b32ab3b50afa783
Pascal Diehm <pdiehm8@gmail.com>
2024-10-22 11:39:00 +0200

    Added bayern WLAN to pascal-laptop

--- machines/pascal-laptop.nix
+++ machines/pascal-laptop.nix
@@ -2,27 +2,41 @@
   # Set hostname
   networking.hostName = "pascal-laptop";
 
-  # Add network connection
-  networking.networkmanager.ensureProfiles.profiles.home-wifi = {
-    connection = {
-      id = "Home WiFi";
-      type = "wifi";
+  # Add network connections
+  networking.networkmanager.ensureProfiles.profiles = {
+    bayern-wlan = {
+      connection = {
+        id = "BayernWLAN";
+        type = "wifi";
+      };
+
+      wifi = {
+        mode = "infrastructure";
+        ssid = "@BayernWLAN";
+      };
     };
 
-    ipv4 = {
-      address1 = "192.168.1.91/16,192.168.1.1";
-      dns = "192.168.1.88";
-      method = "manual";
-    };
+    home-wifi = {
+      connection = {
+        id = "Home WiFi";
+        type = "wifi";
+      };
 
-    wifi = {
-      mode = "infrastructure";
-      ssid = "$HOME_WIFI_SSID";
-    };
+      ipv4 = {
+        address1 = "192.168.1.91/16,192.168.1.1";
+        dns = "192.168.1.88";
+        method = "manual";
+      };
 
-    wifi-security = {
-      key-mgmt = "wpa-psk";
-      psk = "$HOME_WIFI_PSK";
+      wifi = {
+        mode = "infrastructure";
+        ssid = "$HOME_WIFI_SSID";
+      };
+
+      wifi-security = {
+        key-mgmt = "wpa-psk";
+        psk = "$HOME_WIFI_PSK";
+      };
     };
   };
 }
