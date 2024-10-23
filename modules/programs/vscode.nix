d17cb49714c6f7ba81a86403c727d47f18644c84
Pascal Diehm <pdiehm8@gmail.com>
2024-10-23 12:13:44 +0200

    Revised VSCode settings

--- modules/programs/vscode.nix
+++ modules/programs/vscode.nix
@@ -2,7 +2,7 @@
   home-manager.users.pascal = {
     # Setup dependencies
     fonts.fontconfig.enable = true;
-    home.packages = [ pkgs.jetbrains-mono pkgs.jq pkgs.unzip ];
+    home.packages = [ pkgs.jetbrains-mono pkgs.jq pkgs.nixpkgs-fmt pkgs.unzip ];
 
     # Setup VSCode
     programs.vscode = {
@@ -13,15 +13,15 @@
 
       extensions = with pkgs.vscode-extensions; [
         aaron-bond.better-comments
-        bbenoist.nix
         bmewburn.vscode-intelephense-client
         esbenp.prettier-vscode
+        foxundermoon.shell-format
         github.copilot
         github.copilot-chat
         github.vscode-github-actions
         james-yu.latex-workshop
+        jnoortheen.nix-ide
         ms-azuretools.vscode-docker
-        ms-python.black-formatter
         ms-python.debugpy
         ms-python.isort
         ms-python.python
@@ -33,6 +33,11 @@
         vscodevim.vim
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace (lib.importJSON ../../resources/vscode-extensions.json);
 
+      keybindings = [{
+        key = "shift+enter";
+        command = "-python.execSelectionInTerminal";
+      }];
+
       userSettings = {
         # Editor
         "editor.fontFamily" = "JetBrains Mono";
@@ -41,6 +46,7 @@
         "editor.inlayHints.enabled" = "off";
         "editor.lineNumbers" = "relative";
         "editor.linkedEditing" = true;
+        "editor.tabSize" = 2;
         "explorer.compactFolders" = false;
         "extensions.experimental.affinity" = { "vscodevim.vim" = 1; };
         "extensions.ignoreRecommendations" = true;
@@ -50,6 +56,7 @@
         "terminal.integrated.persistentSessionReviveProcess" = "never";
         "terminal.integrated.showExitAlert" = false;
         "update.showReleaseNotes" = false;
+        "workbench.editorAssociations" = { "*.pdf" = "latex-workshop-pdf-hook"; };
         "workbench.iconTheme" = "material-icon-theme";
         "workbench.startupEditor" = "none";
 
@@ -71,24 +78,32 @@
         "git.inputValidation" = true;
         "git.openRepositoryInParentFolders" = "always";
 
+        # LaTeX
+        "latex-workshop.latex.autoClean.run" = "onSucceeded";
+
+        # PHP
+        "intelephense.format.braces" = "k&r";
+
         # Prettier
         "prettier.arrowParens" = "avoid";
         "prettier.bracketSameLine" = true;
         "prettier.printWidth" = 150;
 
-        # PHP
-        "intelephense.format.braces" = "k&r";
-
         # Python
-        "black-formatter.args" = [ "--line-length" "150" ];
+        "autopep8.args" = [ "--indent-size=2" "--max-line-length=150" ];
         "python.analysis.autoFormatStrings" = true;
         "python.analysis.autoImportCompletions" = true;
         "python.analysis.typeCheckingMode" = "standard";
 
         # Languages
-        "[json]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; "editor.tabSize" = 2; };
-        "[jsonc]" = { "editor.defaultFormatter" = "esbenp.prettier-vscode"; "editor.tabSize" = 2; };
-        "[nix]" = { "editor.tabSize" = 2; };
+        "[css]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[dockerfile]"."editor.defaultFormatter" = "ms-azuretools.vscode-docker";
+        "[html]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[javascript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[json]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[jsonc]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[typescript]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "[typescriptreact]"."editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
     };
   };
