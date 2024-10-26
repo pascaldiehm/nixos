6df1bf34aef64e4e664e1836b59d000d9ba3192e
Pascal Diehm <pdiehm8@gmail.com>
2024-10-26 20:27:52 +0200

    Automatically delete home manager backups

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -1,6 +1,6 @@
-{ config, ... }: {
+{ config, helpers, ... }: {
   home-manager = {
-    backupFileExtension = "bak";
+    backupFileExtension = "hm-bak";
     useGlobalPkgs = true;
     useUserPackages = true;
 
@@ -8,6 +8,7 @@
       programs.home-manager.enable = true;
 
       home = {
+        activation.deleteBackups = helpers.mkHomeManagerActivation [ "writeBoundary" ] "run rm -rf $(find $HOME -name '*.hm-bak')";
         homeDirectory = "/home/pascal";
         stateVersion = config.system.stateVersion;
         username = "pascal";
