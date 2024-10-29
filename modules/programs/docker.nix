4bde48dc3f236933a5813ff268206e426d8158b5
Pascal Diehm <pdiehm8@gmail.com>
2024-10-29 09:03:51 +0100

    Revert "Enabled docker auto pruning"
    This reverts commit 49287bc5303431121b24f3866da23b0300efe214.

--- modules/programs/docker.nix
+++ modules/programs/docker.nix
@@ -1,13 +1,6 @@
 { ... }: {
-  # Setup Docker
-  virtualisation.docker = {
-    enable = true;
-
-    autoPrune = {
-      enable = true;
-      flags = [ "--all" ];
-    };
-  };
+  # Enable Docker
+  virtualisation.docker.enable = true;
 
   # Add user to docker group
   users.users.pascal.extraGroups = [ "docker" ];
