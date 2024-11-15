8305cb6227a3d3e24603420d3bd8b5574055540a
Pascal Diehm <pdiehm8@gmail.com>
2024-11-15 22:14:42 +0100

    Enabled hidden files in dolphin

--- modules/programs/dolphin.nix
+++ modules/programs/dolphin.nix
@@ -1,6 +1,12 @@
 { ... }: {
-  home-manager.users.pascal.programs.plasma.configFile.dolphinrc = {
-    General.RememberOpenedTabs = false;
-    IconsMode.PreviewSize = 80;
+  home-manager.users.pascal.programs.plasma = {
+    configFile.dolphinrc = {
+      General.RememberOpenedTabs = true;
+      IconsMode.PreviewSize = 80;
+    };
+
+    dataFile."dolphin/view_properties/global/.directory" = {
+      Settings.HiddenFilesShown = true;
+    };
   };
 }
