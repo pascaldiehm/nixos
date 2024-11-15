b97140901f050c9394338905fbf63778f3300562
Pascal Diehm <pdiehm8@gmail.com>
2024-11-15 22:14:54 +0100

    Added custom konsolestaterc without menubar

--- modules/programs/konsole.nix
+++ modules/programs/konsole.nix
@@ -1,6 +1,10 @@
 { ... }: {
-  home-manager.users.pascal.programs.konsole = {
-    enable = true;
-    extraConfig.KonsoleWindow.RememberWindowSize = false;
+  home-manager.users.pascal = {
+    xdg.stateFile.konsolestaterc.source = ../../resources/konsolestaterc;
+
+    programs.konsole = {
+      enable = true;
+      extraConfig.KonsoleWindow.RememberWindowSize = false;
+    };
   };
 }
