e3acc09815c50d2ef772d081726a5202c2ed2909
Pascal Diehm <pdiehm8@gmail.com>
2024-11-28 10:09:10 +0100

    Added wireguard connection to pascal-laptop

--- machines/pascal-laptop.nix
+++ machines/pascal-laptop.nix
@@ -2,27 +2,52 @@
   networking = {
     hostName = "pascal-laptop";
 
-    networkmanager.ensureProfiles.profiles.home-wifi = {
-      connection = {
-        autoconnect-priority = 50;
-        id = "Home WiFi";
-        type = "wifi";
+    networkmanager.ensureProfiles.profiles = {
+      home = {
+        wireguard.private-key = "$WIREGUARD_PASCAL_LAPTOP_KEY";
+
+        connection = {
+          autoconnect = false;
+          id = "Home";
+          interface-name = "wg0";
+          type = "wireguard";
+        };
+
+        ipv4 = {
+          address1 = "10.42.0.3/24";
+          method = "manual";
+        };
+
+        "wireguard-peer.8TEjIXVJSJryKAeB2L3BTZjaiQZ77KVoaIpdceEZoGg=" = {
+          allowed-ips = "10.42.0.0/24;192.168.0.0/16";
+          endpoint = "$WIREGUARD_ENDPOINT";
+          preshared-key = "$WIREGUARD_PASCAL_LAPTOP_PSK";
+          preshared-key-flags = 0;
+        };
       };
 
-      ipv4 = {
-        address1 = "192.168.1.91/16,192.168.1.1";
-        dns = "192.168.1.88";
-        method = "manual";
-      };
+      home-wifi = {
+        connection = {
+          autoconnect-priority = 50;
+          id = "Home WiFi";
+          type = "wifi";
+        };
 
-      wifi = {
-        mode = "infrastructure";
-        ssid = "$HOME_WIFI_SSID";
-      };
+        ipv4 = {
+          address1 = "192.168.1.91/16,192.168.1.1";
+          dns = "192.168.1.88";
+          method = "manual";
+        };
 
-      wifi-security = {
-        key-mgmt = "wpa-psk";
-        psk = "$HOME_WIFI_PSK";
+        wifi = {
+          mode = "infrastructure";
+          ssid = "$HOME_WIFI_SSID";
+        };
+
+        wifi-security = {
+          key-mgmt = "wpa-psk";
+          psk = "$HOME_WIFI_PSK";
+        };
       };
     };
   };
