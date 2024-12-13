9009dee9b8c253405e3a012b0382ff428917b341
Pascal Diehm <pdiehm8@gmail.com>
2024-12-13 13:19:49 +0100

    Fixed dolphin module layout

--- modules/programs/dolphin.nix
+++ modules/programs/dolphin.nix
@@ -1,12 +1,10 @@
 { ... }: {
   home-manager.users.pascal.programs.plasma = {
+    dataFile."dolphin/view_properties/global/.directory".Settings.HiddenFilesShown = true;
+
     configFile.dolphinrc = {
       General.RememberOpenedTabs = false;
       IconsMode.PreviewSize = 80;
-    };
-
-    dataFile."dolphin/view_properties/global/.directory" = {
-      Settings.HiddenFilesShown = true;
     };
   };
 }
