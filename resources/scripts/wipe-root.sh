d1d9511a2e9be8c038ad090976d82fbf2585b210
Pascal Diehm <pdiehm8@gmail.com>
2024-12-13 13:20:04 +0100

    Delete root history after 30 days

--- resources/scripts/wipe-root.sh
+++ resources/scripts/wipe-root.sh
@@ -3,12 +3,11 @@
 tmp="$(mktemp -d)"
 mount /dev/mapper/nixos "$tmp"
 
-if [ -d "$tmp/root" ]; then
-  mkdir -p "$tmp/history"
-  timestamp="$(date -d "@$(stat -c %Y "$tmp/root")" "+%Y-%m-%d_%H:%M:%S")"
-  mv "$tmp/root" "$tmp/history/$timestamp"
-fi
+mkdir -p "$tmp/history"
+[ -d "$tmp/root" ] && mv "$tmp/root" "$tmp/history/$(date -d "@$(stat -c %Y "$tmp/root")" "+%Y-%m-%d_%H:%M:%S")"
 
+find "$tmp/history" -maxdepth 1 -mtime +30 -exec rm -rf {} \;
 btrfs subvolume create "$tmp/root"
+
 umount "$tmp"
 rmdir "$tmp"
