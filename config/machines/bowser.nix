8360e8e72b34ba25ab43506800eea417b5d0de65
Pascal Diehm <pdiehm8@gmail.com>
2025-01-05 14:24:52 +0100

    Added wireguard configuration

--- config/machines/bowser.nix
+++ config/machines/bowser.nix
@@ -10,10 +10,42 @@
     '';
   };
 
-  systemd.network.networks."10-wired" = {
-    address = [ "192.168.1.88/16" ];
-    gateway = [ "192.168.1.1" ];
-    linkConfig.RequiredForOnline = "routable";
-    matchConfig.Name = "eth0";
+  sops.secrets = {
+    "wireguard/bowser/key".owner = "systemd-network";
+    "wireguard/bowser/goomba".owner = "systemd-network";
+  };
+
+  systemd.network = {
+    netdevs."50-wg" = {
+      wireguardConfig.PrivateKeyFile = config.sops.secrets."wireguard/bowser/key".path;
+
+      netdevConfig = {
+        Kind = "wireguard";
+        Name = "wg";
+      };
+
+      wireguardPeers = [{
+        AllowedIPs = [ "10.42.0.0/24" ];
+        Endpoint = "116.203.102.96:51820";
+        PersistentKeepalive = 25;
+        PresharedKeyFile = config.sops.secrets."wireguard/bowser/goomba".path;
+        PublicKey = "8TEjIXVJSJryKAeB2L3BTZjaiQZ77KVoaIpdceEZoGg=";
+      }];
+    };
+
+    networks = {
+      "10-wired" = {
+        address = [ "192.168.1.88/16" ];
+        gateway = [ "192.168.1.1" ];
+        linkConfig.RequiredForOnline = "routable";
+        matchConfig.Name = "eth0";
+      };
+
+      "50-wg" = {
+        address = [ "10.42.0.2/24" ];
+        matchConfig.Name = "wg";
+        networkConfig.IPMasquerade = "ipv4";
+      };
+    };
   };
 }
--- config/machines/goomba.nix
+++ config/machines/goomba.nix
@@ -1,3 +1,50 @@
-{ ... }: {
+{ config, ... }: {
   users.users.pascal.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOcME9MFgLx87iigjoZHIodQs2Rre39SflSRCfnpFZs pascal" ];
+
+  sops.secrets = {
+    "wireguard/goomba/key".owner = "systemd-network";
+    "wireguard/goomba/bowser".owner = "systemd-network";
+    "wireguard/goomba/pascal-laptop".owner = "systemd-network";
+    "wireguard/goomba/pascal-phone".owner = "systemd-network";
+  };
+
+  systemd.network = {
+    netdevs."50-wg" = {
+      netdevConfig = {
+        Kind = "wireguard";
+        Name = "wg";
+      };
+
+      wireguardConfig = {
+        ListenPort = 51820;
+        PrivateKeyFile = config.sops.secrets."wireguard/goomba/key".path;
+      };
+
+      wireguardPeers = [
+        {
+          AllowedIPs = [ "10.42.0.2/32" "192.168.0.0/24" ];
+          PresharedKeyFile = config.sops.secrets."wireguard/goomba/bowser".path;
+          PublicKey = "DGhb5LNEW6X+WhVhzkuUi3wpyYuDDDNq1TQDze4cCTk=";
+        }
+
+        {
+          AllowedIPs = [ "10.42.0.3/32" ];
+          PresharedKeyFile = config.sops.secrets."wireguard/goomba/pascal-laptop".path;
+          PublicKey = "NcWpXbnJs4bn9QvhIvCfM/9ocp1JYKSL4DLxDskhIRg=";
+        }
+
+        {
+          AllowedIPs = [ "10.42.0.4/32" ];
+          PresharedKeyFile = config.sops.secrets."wireguard/goomba/pascal-phone".path;
+          PublicKey = "dYBJTDmUJCEMB6apLXdgktkRkQrtUvi7up5SFismomI=";
+        }
+      ];
+    };
+
+    networks."50-wg" = {
+      address = [ "10.42.0.1/24" ];
+      matchConfig.Name = "wg";
+      networkConfig.IPv4Forwarding = true;
+    };
+  };
 }
