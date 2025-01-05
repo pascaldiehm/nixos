c62d00b00baec9b9c132a46fb84b5b8c205a32ad
Pascal Diehm <pdiehm8@gmail.com>
2025-01-05 15:07:30 +0100

    Added RAID configuration to bowser

--- config/machines/bowser.nix
+++ config/machines/bowser.nix
@@ -1,3 +1,12 @@
-{ ... }: {
+{ config, pkgs, ... }: {
   users.users.pascal.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB0QlzxdW4Pxbb7vcF6C88+0aySGjhFO//QRBB5rv4JY pascal" ];
+
+  boot.swraid = {
+    enable = true;
+
+    mdadmConf = ''
+      PROGRAM ${pkgs.curl}/bin/curl -s -H "Authorization: Bearer $(cat ${config.sops.secrets.ntfy.path})" -d 'A RAID drive has failed' 'https://ntfy.pdiehm.dev/bowser-raid'
+      ARRAY /dev/md/0 metadata=1.2 name=bowser:0 UUID=d56224b5:9d97fe09:73ab00f5:631ed84c
+    '';
+  };
 }
