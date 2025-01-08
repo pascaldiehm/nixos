399e04158fa6a93d53aca2d9027cb3beb822668f
Pascal Diehm <pdiehm8@gmail.com>
2025-01-08 15:36:54 +0100

    Reworked ntfy message for md monitor events

--- config/machines/bowser.nix
+++ config/machines/bowser.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     mdadmConf = ''
-      PROGRAM ${helpers.ntfy "raid" "A RAID drive has failed"}
+      PROGRAM ${helpers.ntfy "raid" "$1: $2\${3:+ ($3)}"}
       ARRAY /dev/md/0 metadata=1.2 name=bowser:0 UUID=d56224b5:9d97fe09:73ab00f5:631ed84c
     '';
   };
