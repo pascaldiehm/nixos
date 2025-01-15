302e03a726225362564fc0014d41f86d91c6a114
Pascal Diehm <pdiehm8@gmail.com>
2025-01-15 10:36:05 +0100

    Wait for printer before configuring ipp-everywhere

--- config/machines/pascal-pc.nix
+++ config/machines/pascal-pc.nix
@@ -55,6 +55,8 @@
       wants = [ "cups.service" ];
 
       script = ''
+        while ! ${pkgs.curl}/bin/curl "localhost:60000/ipp/print"; do sleep 10; done
+
         ${pkgs.cups}/bin/lpadmin -D "Brother DCP-J1050DW" -m everywhere -o "ColorMode=Gray" -o "Duplex=DuplexNoTumble" -o "PageSize=A4" -p Brother_DCP-J1050DW -v "ipp://localhost:60000/ipp/print" -E || true
         ${pkgs.cups}/bin/lpadmin -d Brother_DCP-J1050DW
         systemctl stop cups.service
