a9be4370a57155c17ff0baae384c880901257e9e
Pascal Diehm <pdiehm8@gmail.com>
2025-01-16 14:22:10 +0100

    Removed redundant mime app definitions

--- base/desktop/xdg.nix
+++ base/desktop/xdg.nix
@@ -2,17 +2,6 @@
   home-manager.users.pascal.xdg = {
     stateFile.konsolestaterc.source = ../../resources/konsolestaterc;
 
-    mimeApps = {
-      enable = true;
-
-      defaultApplications = {
-        "text/html" = "firefox.desktop";
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "x-scheme-handler/mailto" = "thunderbird.desktop";
-      };
-    };
-
     userDirs = {
       enable = true;
       createDirectories = true;
