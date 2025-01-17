f435b7ca76f75e866981c0b14ee45b99f35cfa2d
Pascal Diehm <pdiehm8@gmail.com>
2025-01-17 09:49:07 +0100

    Check if patches still apply after upgrade

--- patches/default.nix
+++ patches/default.nix
@@ -2,7 +2,7 @@
   nixpkgs.overlays = [
     (self: super: {
       nixfmt-rfc-style = super.nixfmt-rfc-style.overrideAttrs {
-        patches = [ nixfmt/compact-lists.patch nixfmt/compact-params.patch ];
+        patches = [ nixfmt-rfc-style/compact-lists.patch nixfmt-rfc-style/compact-params.patch ];
       };
     })
   ];
