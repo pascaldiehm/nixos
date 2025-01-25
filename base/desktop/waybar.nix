c1efd8ab090f421a8d8fdac50cd715085fad65e6
Pascal Diehm <pdiehm8@gmail.com>
2025-01-25 12:15:33 +0100

    Configured modules for waybar

--- base/desktop/waybar.nix
+++ base/desktop/waybar.nix
@@ -1,28 +1,78 @@
-{
+{ lib, pkgs, ... }: {
   home-manager.users.pascal.programs.waybar = {
     enable = true;
     systemd.enable = true;
 
-    # TODO https://github.com/Alexays/Waybar/wiki
-    settings.mainBar = {
+    # TODO https://github.com/Alexays/Waybar/wiki/Styling
+    settings.bar = {
+      clock.tooltip-format = "<tt>{calendar}</tt>";
       modules-center = [ "clock" ];
-      modules-left = [ "hyprland/workspaces" ];
-      modules-right = [ "group/hardware" "group/screen" "wireplumber" "network" "group/battery" "group/poweroff" ];
+      modules-left = [ "privacy" "hyprland/workspaces" ];
+      modules-right = [ "group/hardware" "group/screen" "wireplumber" "network" "battery" "group/session" ];
 
-      "group/battery" = {
-        drawer.transition-left-to-right = false;
-        modules = [ "battery" "power-profiles-daemon" ];
-        orientation = "inherit";
+      backlight = {
+        format = "{icon} {percent}%";
+        format-icons = [ "󰃞" "󰃟" "󰃠" ];
+        on-click = "${lib.getExe pkgs.brightnessctl} s 50%";
+        on-scroll-down = "${lib.getExe pkgs.brightnessctl} s 1%-";
+        on-scroll-up = "${lib.getExe pkgs.brightnessctl} s +1%";
+        tooltip = false;
+      };
+
+      battery = {
+        format = "{icon} {capacity}%";
+        format-charging = "󰂄 {capacity}%";
+        format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+        interval = 10;
+        tooltip-format = "{timeTo}";
+
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+      };
+
+      cpu = {
+        format = " {usage}%";
+
+        states = {
+          load = 10;
+          high = 80;
+        };
+      };
+
+      "custom/lock" = {
+        format = "󰌾";
+        on-click = "${lib.getExe' pkgs.systemd "loginctl"} lock-session";
+        tooltip-format = "Lock screen";
+      };
+
+      "custom/reboot" = {
+        format = "󰜉";
+        on-click = "${lib.getExe' pkgs.systemd "systemctl"} reboot";
+        tooltip-format = "Reboot";
+      };
+
+      "custom/shutdown" = {
+        format = "󰐥";
+        on-click = "${lib.getExe' pkgs.systemd "systemctl"} poweroff";
+        tooltip-format = "Power off";
+      };
+
+      "custom/sleep" = {
+        format = "󰤄";
+        on-click = "${lib.getExe' pkgs.systemd "systemctl"} suspend";
+        tooltip-format = "Sleep";
+      };
+
+      disk = {
+        format = " {percentage_used}%";
+        tooltip-format = "{specific_used:.1f} / {specific_total:.1f} GiB";
+        unit = "GiB";
       };
 
       "group/hardware" = {
         modules = [ "cpu" "memory" "disk" "temperature" ];
-        orientation = "inherit";
-      };
-
-      "group/poweroff" = {
-        drawer.transition-left-to-right = false;
-        modules = [ "custom/shutdown" "custom/lock" "custom/reboot" ];
         orientation = "inherit";
       };
 
@@ -32,9 +82,58 @@
         orientation = "inherit";
       };
 
+      "group/session" = {
+        drawer.transition-left-to-right = false;
+        modules = [ "custom/shutdown" "custom/lock" "custom/sleep" "custom/reboot" ];
+        orientation = "inherit";
+      };
+
+      idle_inhibitor = {
+        format = "{icon}";
+        tooltip = false;
+
+        format-icons = {
+          activated = "󰒳";
+          deactivated = "󰒲";
+        };
+      };
+
       "hyprland/workspaces" = {
         all-outputs = true;
         move-to-monitor = true;
+      };
+
+      memory = {
+        format = " {percentage}%";
+        interval = 10;
+        tooltip-format = "{used:.1f} / {total:.1f} GiB";
+      };
+
+      network = {
+        format = "󰛵";
+        format-disconnected = "󰲛";
+        format-ethernet = "󰈀 {ipaddr}/{cidr}";
+        format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
+        format-wifi = "{icon} {ipaddr}/{cidr}";
+        interval = 10;
+        tooltip-format = "Unknown state";
+        tooltip-format-disconnected = "Disconnected";
+        tooltip-format-ethernet = "{ifname}: Ethernet";
+        tooltip-format-wifi = "{ifname}: {essid} ({frequency}GHz WiFi)";
+      };
+
+      temperature = {
+        format = "{icon} {temperatureC}°C";
+        format-icons = [ "" "" "" "" "" ];
+        tooltip = false;
+      };
+
+      wireplumber = {
+        format = "{icon} {volume}%";
+        format-icons = [ "󰕿" "󰖀" "󰕾" ];
+        format-muted = "󰖁";
+        on-click = "${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_SINK@ toggle";
+        on-click-right = "${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_SINK@ 40%";
       };
     };
   };
