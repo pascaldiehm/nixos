ad318d4ce1b8e9dab2325fbfa122649c5bf2cc07
Pascal Diehm <pdiehm8@gmail.com>
2025-01-26 14:28:41 +0100

    Properly include previous attributes in overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,13 @@
 {
   nixpkgs.overlays = [
     (self: super: {
-      nixfmt-rfc-style = super.nixfmt-rfc-style.overrideAttrs {
-        patches = (super.patches or [ ]) ++ [ nixfmt-rfc-style/compact-lists.patch nixfmt-rfc-style/compact-params.patch ];
-      };
+      nixfmt-rfc-style = super.nixfmt-rfc-style.overrideAttrs (prev: {
+        patches = (prev.patches or [ ]) ++ [ nixfmt-rfc-style/compact-lists.patch nixfmt-rfc-style/compact-params.patch ];
+      });
 
       vimPlugins = super.vimPlugins // {
-        oil-nvim = super.vimPlugins.oil-nvim.overrideAttrs (attrs: {
-          postPatch = (attrs.postPatch or "") + "rm -f doc/recipes.md";
+        oil-nvim = super.vimPlugins.oil-nvim.overrideAttrs (prev: {
+          postPatch = (prev.postPatch or "") + "rm -f doc/recipes.md";
         });
       };
     })
