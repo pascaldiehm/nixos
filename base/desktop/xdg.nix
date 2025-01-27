a2acb5b3f6cc13ecd6a2f2cdcb2a47e10b6bf920
Pascal Diehm <pdiehm8@gmail.com>
2025-01-27 11:42:59 +0100

    Also removed desktop user directory

--- base/desktop/xdg.nix
+++ base/desktop/xdg.nix
@@ -2,6 +2,7 @@
   home-manager.users.pascal.xdg.userDirs = {
     enable = true;
     createDirectories = true;
+    desktop = null;
     music = null;
     pictures = null;
     publicShare = null;
