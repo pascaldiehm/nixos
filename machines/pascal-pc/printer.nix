517f605fb7104e5ac8096427373fa790e8c1f33f
Pascal Diehm <pdiehm8@gmail.com>
2025-01-29 12:58:54 +0100

    Removed color setting from printer

--- machines/pascal-pc/printer.nix
+++ machines/pascal-pc/printer.nix
@@ -27,7 +27,7 @@
     script = ''
       while ! ${lib.getExe pkgs.curl} "localhost:60000/ipp/print" 2> /dev/null; do sleep 10; done
 
-      ${lib.getExe' pkgs.cups "lpadmin"} -D "Brother DCP-J1050DW" -m everywhere -o ColorMode=Gray -o Duplex=DuplexNoTumble -o PageSize=A4 -p Brother_DCP-J1050DW -v ipp://localhost:60000/ipp/print -E || true
+      ${lib.getExe' pkgs.cups "lpadmin"} -D "Brother DCP-J1050DW" -m everywhere -o Duplex=DuplexNoTumble -o PageSize=A4 -p Brother_DCP-J1050DW -v ipp://localhost:60000/ipp/print -E || true
       ${lib.getExe' pkgs.cups "lpadmin"} -d Brother_DCP-J1050DW
       systemctl stop cups.service
     '';
