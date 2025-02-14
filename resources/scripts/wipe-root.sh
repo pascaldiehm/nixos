305179661c0b1d108b1c96a6e2c9637752bc31df
Pascal Diehm <pdiehm8@gmail.com>
2025-02-14 16:01:02 +0100

    Clarified history cleaning in wipe-root script

--- resources/scripts/wipe-root.sh
+++ resources/scripts/wipe-root.sh
@@ -7,7 +7,7 @@ mkdir -p "$tmp/history"
 [ -d "$tmp/root" ] && mv "$tmp/root" "$tmp/history/$(date -d "@$(stat -c "%Y" "$tmp/root")" "+%Y-%m-%d_%H:%M:%S")"
 btrfs subvolume create "$tmp/root"
 
-find "$tmp/history" -maxdepth 1 -mtime +30 | while read -r dir; do
+find "$tmp/history" -mindepth 1 -maxdepth 1 -mtime +30 | while read -r dir; do
   chattr -i "$dir/var/empty"
   rm -rf "$dir"
 done
