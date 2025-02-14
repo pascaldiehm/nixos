80a75c42ac0136db59f63a58c53fd30499dd4692
Pascal Diehm <pdiehm8@gmail.com>
2025-02-14 15:57:10 +0100

    Removed unused persistence hideMounts attribut

--- base/common/persistence.nix
+++ base/common/persistence.nix
@@ -2,7 +2,6 @@
   environment.persistence."/perm" = {
     directories = [ "/etc/nixos" "/var/lib/nixos" "/var/lib/systemd" ];
     files = [ "/etc/machine-id" ];
-    hideMounts = true;
 
     users.pascal.directories = lib.mapAttrsToList (directory: mode: { inherit directory mode; }) {
       ".config/nixos" = "0755";
