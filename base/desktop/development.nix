fb2f9af15d11bbd8a54307ca18a6a9b9e3a1972f
Pascal Diehm <pdiehm8@gmail.com>
2025-02-16 18:09:34 +0100

    Manually specified required texlive packages

--- base/desktop/development.nix
+++ base/desktop/development.nix
@@ -1,8 +1,7 @@
 { pkgs, ... }: {
   home-manager.users.pascal = {
-    programs.java.enable = true;
-
     home = {
+      packages = [ pkgs.cmake pkgs.gcc pkgs.gdb pkgs.gnumake pkgs.ninja pkgs.nodejs pkgs.php pkgs.python3 ];
       sessionVariables.CMAKE_GENERATOR = "Ninja";
 
       file = {
@@ -10,18 +9,25 @@
         "Documents/.clang-format".source = ../../resources/clang/format.yaml;
         "Documents/.clang-tidy".source = ../../resources/clang/tidy.yaml;
       };
+    };
 
-      packages = [
-        pkgs.cmake
-        pkgs.gcc
-        pkgs.gdb
-        pkgs.gnumake
-        pkgs.ninja
-        pkgs.nodejs
-        pkgs.php
-        pkgs.python3
-        pkgs.texlive.combined.scheme-full
-      ];
+    programs = {
+      java.enable = true;
+
+      texlive = {
+        enable = true;
+
+        extraPackages = tpkgs: {
+          inherit (tpkgs)
+            collection-bibtexextra
+            collection-fontsrecommended
+            collection-langgerman
+            collection-latexrecommended
+            csquotes
+            pgfplots
+            ;
+        };
+      };
     };
   };
 }
