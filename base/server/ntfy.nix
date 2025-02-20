bf4f4411811b9487b936a478cd694a244a6a171b
Pascal Diehm <pdiehm8@gmail.com>
2025-02-20 10:20:53 +0100

    Assign ntfy secret to user

--- base/server/ntfy.nix
+++ base/server/ntfy.nix
@@ -1,6 +1,6 @@
 { config, machine, pkgs, ... }: {
   home-manager.users.pascal.home.packages = [ pkgs.scripts.ntfy ];
-  sops.secrets."${machine.name}/ntfy" = { };
+  sops.secrets."${machine.name}/ntfy".owner = "pascal";
 
   nixpkgs.overlays = [
     (self: super: {
@@ -9,11 +9,6 @@
         runtimeInputs = [ self.curl ];
 
         text = ''
-          if [ $# != 2 ]; then
-            echo "Usage: ntfy <channel> <message>"
-            exit 1
-          fi
-
           TOKEN="$(cat "${config.sops.secrets."${machine.name}/ntfy".path}")"
           curl -s -H "Authorization: Bearer $TOKEN" -d "$2" "https://ntfy.pdiehm.dev/${machine.name}-$1"
         '';
