bd8816dfb525e3342f34fae36483621fa3133d94
Pascal Diehm <pdiehm8@gmail.com>
2025-02-21 13:08:30 +0100

    Setup pihole as local DNS cache

--- base/desktop/firefox.nix
+++ base/desktop/firefox.nix
@@ -21,6 +21,7 @@
         "Matrix Admin" = "https://admin.etke.cc";
         Netflix = "https://netflix.com";
         PayPal = "https://paypal.com";
+        PiHole = "http://192.168.1.88:8072/admin";
         Sparkasse = "https://sparkasse-mainfranken.de";
 
         _toolbar = {
--- machines/pascal-pc/networking.nix
+++ machines/pascal-pc/networking.nix
@@ -2,12 +2,13 @@
   networking.networkmanager.ensureProfiles.profiles.wired = {
     connection = {
       autoconnect-priority = 90;
-      id = "Wired connection";
+      id = "Wired";
       type = "ethernet";
     };
 
     ipv4 = {
       address1 = "192.168.1.90/16,192.168.1.1";
+      dns = "192.168.1.88";
       method = "manual";
     };
   };
