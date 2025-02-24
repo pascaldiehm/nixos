92e8c533aeb6c1d871742f337228915e443259c8
Pascal Diehm <pdiehm8@gmail.com>
2025-02-24 08:45:40 +0100

    Persist entire ssh config directory on servers

--- base/server/persistence.nix
+++ base/server/persistence.nix
@@ -1,13 +1,6 @@
 {
   environment.persistence."/perm" = {
-    directories = [ "/var/lib/docker" "/var/lib/fail2ban" ];
+    directories = [ "/etc/ssh" "/var/lib/docker" "/var/lib/fail2ban" ];
     users.pascal.directories = [ "docker" ];
-
-    files = [
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-    ];
   };
 }
