b34681ba7c2c09892b18377070bd4af63e7e5aa1
Pascal Diehm <pdiehm8@gmail.com>
2025-03-14 17:48:54 +0100

    Changed journalwatch network dependency to requires

--- base/server/journalwatch.nix
+++ base/server/journalwatch.nix
@@ -3,8 +3,8 @@
     after = [ "network-online.target" ];
     description = "Watch journalctl and report security-relevant events to ntfy";
     path = [ pkgs.jq pkgs.scripts.ntfy ];
+    requires = [ "network-online.target" ];
     script = lib.readFile ../../resources/scripts/journalwatch.sh;
     wantedBy = [ "multi-user.target" ];
-    wants = [ "network-online.target" ];
   };
 }
