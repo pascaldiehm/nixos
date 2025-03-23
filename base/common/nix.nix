ddcc04edc2827e75fdb528fdc608a1dc183437cd
Pascal Diehm <pdiehm8@gmail.com>
2025-03-23 22:58:44 +0100

    Added local binary cache

--- base/common/nix.nix
+++ base/common/nix.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ inputs, lib, ... }: {
   nix = {
     channel.enable = false;
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
@@ -13,7 +13,32 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "flakes" "nix-command" "pipe-operators" ];
+      substituters = lib.mkForce [ "http://127.0.0.1:5778" ];
       use-xdg-base-directories = true;
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts.nix-subsituter-proxy = {
+      locations = {
+        "@fallback".proxyPass = "https://cache.nixos.org";
+
+        "/" = {
+          proxyPass = "http://bowser:5779";
+
+          extraConfig = ''
+            error_page 502 504 = @fallback;
+            proxy_connect_timeout 100ms;
+          '';
+        };
+      };
+
+      listen = lib.singleton {
+        addr = "127.0.0.1";
+        port = 5778;
+      };
     };
   };
 }
