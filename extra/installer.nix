c3c46d7472ebc5aa93f04a611eca68d28dcc317e
Pascal Diehm <pdiehm8@gmail.com>
2025-03-25 10:03:36 +0100

    Added nix cache to installer image

--- extra/installer.nix
+++ extra/installer.nix
@@ -1,10 +1,14 @@
 { lib, modulesPath, pkgs, ... }: {
-  imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ];
+  imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ../modules/nixcache.nix ];
   console.keyMap = "de";
   environment.systemPackages = [ (pkgs.writeShellScriptBin "nixinstall" "nix run github:pascaldiehm/nixos#install") ];
-  nix.settings.experimental-features = [ "flakes" "nix-command" "pipe-operators" ];
   nixpkgs.hostPlatform = "x86_64-linux";
   services.getty.helpLine = lib.mkAfter "Run 'nixinstall' to install NixOS.";
+
+  nix = {
+    cache = "http://192.168.1.88:5779";
+    settings.experimental-features = [ "flakes" "nix-command" "pipe-operators" ];
+  };
 
   users.users.nixos.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMVpBDeuHacnZcYsM06F+ktipogjcLNZrL6rjYwZIV51 pascal"
