fe65a3640e65e176a54ab8c492a1c60fd9816f42
Pascal Diehm <pdiehm8@gmail.com>
2025-03-31 23:09:55 +0200

    Added user service for thunderbird notifications

--- base/desktop/thunderbird.nix
+++ base/desktop/thunderbird.nix
@@ -1,11 +1,19 @@
-{ lib, pkgs, ... }: {
-  home-manager.users.pascal.programs.thunderbird = {
-    enable = true;
-    profiles.default.isDefault = true;
-    settings."mail.collect_email_address_outgoing" = false;
+{ config, lib, pkgs, ... }: {
+  home-manager.users.pascal = {
+    programs.thunderbird = {
+      enable = true;
+      profiles.default.isDefault = true;
+      settings."mail.collect_email_address_outgoing" = false;
 
-    package = pkgs.thunderbird.override {
-      extraPolicies.ExtensionSettings = lib.mkMozillaExtensions ../../resources/extensions/thunderbird.json;
+      package = pkgs.thunderbird.override {
+        extraPolicies.ExtensionSettings = lib.mkMozillaExtensions ../../resources/extensions/thunderbird.json;
+      };
+    };
+
+    systemd.user.services.thunderbird-notify = {
+      Install.WantedBy = [ "default.target" ];
+      Service.ExecStart = "${lib.getExe config.home-manager.users.pascal.programs.thunderbird.package} --headless";
+      Unit.Description = "Thunderbird notification daemon";
     };
   };
 }
