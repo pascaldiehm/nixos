3da173f8d79e68a980985a2d8cbb60e1d2bec2c1
Pascal Diehm <pdiehm8@gmail.com>
2025-04-01 10:31:44 +0200

    Revert "Added user service for thunderbird notifications"
    This reverts commit fe65a3640e65e176a54ab8c492a1c60fd9816f42.

--- base/desktop/thunderbird.nix
+++ base/desktop/thunderbird.nix
@@ -1,19 +1,11 @@
-{ config, lib, pkgs, ... }: {
-  home-manager.users.pascal = {
-    programs.thunderbird = {
-      enable = true;
-      profiles.default.isDefault = true;
-      settings."mail.collect_email_address_outgoing" = false;
+{ lib, pkgs, ... }: {
+  home-manager.users.pascal.programs.thunderbird = {
+    enable = true;
+    profiles.default.isDefault = true;
+    settings."mail.collect_email_address_outgoing" = false;
 
-      package = pkgs.thunderbird.override {
-        extraPolicies.ExtensionSettings = lib.mkMozillaExtensions ../../resources/extensions/thunderbird.json;
-      };
-    };
-
-    systemd.user.services.thunderbird-notify = {
-      Install.WantedBy = [ "default.target" ];
-      Service.ExecStart = "${lib.getExe config.home-manager.users.pascal.programs.thunderbird.package} --headless";
-      Unit.Description = "Thunderbird notification daemon";
+    package = pkgs.thunderbird.override {
+      extraPolicies.ExtensionSettings = lib.mkMozillaExtensions ../../resources/extensions/thunderbird.json;
     };
   };
 }
