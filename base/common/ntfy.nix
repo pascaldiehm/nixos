c19a40ae7cbffcd17760900f65ae40cfa5c88908
Pascal Diehm <pdiehm8@gmail.com>
2025-04-12 20:11:38 +0200

    Added default channel to ntfy command

--- base/common/ntfy.nix
+++ base/common/ntfy.nix
@@ -2,13 +2,16 @@
   home-manager.users.pascal.home.packages = [ pkgs.scripts.ntfy ];
 
   programs.scripts.ntfy.text = ''
-    if [ "$#" != "2" ]; then
-      echo "Usage: ntfy <channel> <message>"
+    TOKEN="$(cat ${config.sops.secrets."common/ntfy".path})"
+
+    if [ "$#" = "1" ]; then
+      ${lib.getExe pkgs.curl} -Ss -H "Authorization: Bearer $TOKEN" -d "$1" https://ntfy.pdiehm.dev/default
+    elif [ "$#" = "2" ]; then
+      ${lib.getExe pkgs.curl} -Ss -H "Authorization: Bearer $TOKEN" -d "$2" "https://ntfy.pdiehm.dev/${machine.name}-$1"
+    else
+      echo "Usage: ntfy [channel] <message>"
       exit 1
     fi
-
-    TOKEN="$(cat ${config.sops.secrets."common/ntfy".path})"
-    ${lib.getExe pkgs.curl} -Ss -H "Authorization: Bearer $TOKEN" -d "$2" "https://ntfy.pdiehm.dev/${machine.name}-$1"
   '';
 
   sops.secrets."common/ntfy" = {
