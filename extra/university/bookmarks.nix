8616e665f5df55567c1595a3ec92fda84327aee0
Pascal Diehm <pdiehm8@gmail.com>
2025-04-19 09:47:54 +0200

    Added university user portals to bookmarks

--- extra/university/bookmarks.nix
+++ extra/university/bookmarks.nix
@@ -2,6 +2,8 @@
   home-manager.users.pascal.programs.firefox.profiles.default.bookmarks.settings = lib.mkFirefoxBookmarks {
     "Uni Würzburg" = {
       GitLab = "https://gitlab.informatik.uni-wuerzburg.de";
+      "User Portal" = "https://user-portal.rz.uni-wuerzburg.de";
+      "User Portal (IfI)" = "https://user.informatik.uni-wuerzburg.de";
       WueCampus = "https://wuecampus.uni-wuerzburg.de";
       WueStudy = "https://wuestudy.zv.uni-wuerzburg.de";
     };
