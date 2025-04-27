c715be97c51e8d9eaa836c03e41b479aaafe8046
Pascal Diehm <pdiehm8@gmail.com>
2025-04-27 14:00:12 +0200

    Installed GIMP

--- machines/pascal-pc/programs.nix
+++ machines/pascal-pc/programs.nix
@@ -2,8 +2,7 @@
   home-manager.users.pascal.home.packages = [
     pkgs.blender-hip
     pkgs.freecad-wayland
-    pkgs.kdePackages.kdenlive
-    pkgs.krita
+    pkgs.gimp3
     pkgs.prismlauncher
     pkgs.quickemu
     pkgs.tenacity
