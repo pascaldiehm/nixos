6be490ebacb63e46b5f895e83972f57bcab34065
Pascal Diehm <pdiehm8@gmail.com>
2025-05-23 17:25:37 +0200

    Reduced impermanence history to 7 days

--- resources/scripts/wipe-root.sh
+++ resources/scripts/wipe-root.sh
@@ -7,6 +7,6 @@ mkdir -p "$TMP/history"
 test -d "$TMP/root" && mv "$TMP/root" "$TMP/history/$(date -d "@$(stat -c "%Y" "$TMP/root")" "+%Y-%m-%d_%H:%M:%S")"
 btrfs subvolume create "$TMP/root"
 
-find "$TMP/history" -mindepth 1 -maxdepth 1 -mtime +30 -exec btrfs subvolume delete -R "{}" +
+find "$TMP/history" -mindepth 1 -maxdepth 1 -mtime +7 -exec btrfs subvolume delete -R "{}" +
 umount "$TMP"
 rmdir "$TMP"
