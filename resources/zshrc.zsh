a14c8d08935430420d706c820523407fadccf090
Pascal Diehm <pdiehm8@gmail.com>
2025-06-19 15:03:55 +0200

    Don't force remove result symlink after nixos-test

--- resources/zshrc.zsh
+++ resources/zshrc.zsh
@@ -125,7 +125,7 @@ function nixos-diff() {
 
 function nixos-test() {
   nixos-rebuild --sudo --impure --flake ~/.config/nixos "${1:-test}"
-  test "$1" = "build" || rm -f result
+  test "$1" = "build" || rm result
 }
 
 compdef '_arguments ":mode:(boot build switch test)"' nixos-test
