096e99e1598b4daec5b7b2e1205a6b30d93eeab0
Pascal Diehm <pdiehm8@gmail.com>
2025-06-19 23:32:33 +0200

    Enabled tlp on pascal-laptop

--- machines/pascal-laptop/misc.nix
+++ machines/pascal-laptop/misc.nix
@@ -1,1 +1,4 @@
-{ home-manager.users.pascal.wayland.windowManager.hyprland.settings.monitor = "eDP-1, preferred, auto, 1.33"; }
+{
+  home-manager.users.pascal.wayland.windowManager.hyprland.settings.monitor = "eDP-1, preferred, auto, 1.33";
+  services.tlp.enable = true;
+}
