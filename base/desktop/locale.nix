9ea0a98b22e1e1e6c02b6baad22284aea10628a5
Pascal Diehm <pdiehm8@gmail.com>
2025-06-29 21:31:04 +0200

    Reworked locale settings

--- base/desktop/locale.nix
+++ base/desktop/locale.nix
@@ -1,9 +1,9 @@
 {
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
+    LC_COLLATE = "C.UTF-8";
     LC_MEASUREMENT = "de_DE.UTF-8";
     LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
     LC_PAPER = "de_DE.UTF-8";
     LC_TELEPHONE = "de_DE.UTF-8";
     LC_TIME = "en_DK.UTF-8";
