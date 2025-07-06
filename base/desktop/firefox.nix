914ccfd1b6f353d4d45635f52389cf5ca40172f9
Pascal Diehm <pdiehm8@gmail.com>
2025-07-06 16:56:23 +0200

    Revert "Removed WhatsApp bookmark"
    This reverts commit 3514a7b2ccd14e0c27fef45d2df5e0736b363daa.

--- base/desktop/firefox.nix
+++ base/desktop/firefox.nix
@@ -30,6 +30,7 @@
             Element = "https://app.element.io";
             GitHub = "https://github.com";
             "Home Assistant" = "http://192.168.1.89:8123";
+            WhatsApp = "https://web.whatsapp.com";
 
             Google = {
               Account = "https://myaccount.google.com";
