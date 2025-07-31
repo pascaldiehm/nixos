a0fd5c590e9259f9a549f993b38436c1bc23894c
Pascal Diehm <pdiehm8@gmail.com>
2025-07-31 13:34:42 +0200

    Use absolute path for ZSH dotdir

--- base/common/zsh.nix
+++ base/common/zsh.nix
@@ -5,7 +5,7 @@
     enable = true;
     autosuggestion.enable = true;
     completionInit = "autoload -U compinit && compinit -d /home/pascal/.local/state/zsh/.zcompdump";
-    dotDir = ".config/zsh";
+    dotDir = "/home/pascal/.config/zsh";
     history.path = "/home/pascal/.local/state/zsh/.zsh_history";
     initContent = lib.readFile ../../resources/zshrc.zsh;
     plugins = lib.mapAttrsToList (name: src: { inherit name src; }) { inherit (pkgs) zsh-completions; };
