e9973872d55e3d748a3ec2b6fa9df6b9b1d9ef35
Pascal Diehm <pdiehm8@gmail.com>
2025-07-31 20:42:03 +0200

    Clear history before archiving last root

--- resources/scripts/wipe-root.sh
+++ resources/scripts/wipe-root.sh
@@ -4,9 +4,10 @@ TMP="$(mktemp -d)"
 mount /dev/disk/by-label/nixos "$TMP"
 
 mkdir -p "$TMP/history"
+find "$TMP/history" -mindepth 1 -maxdepth 1 -mtime +7 -exec btrfs subvolume delete -R "{}" +
+
 test -d "$TMP/root" && mv "$TMP/root" "$TMP/history/$(date -d "@$(stat -c "%Y" "$TMP/root")" "+%Y-%m-%d_%H:%M:%S")"
 btrfs subvolume create "$TMP/root"
 
-find "$TMP/history" -mindepth 1 -maxdepth 1 -mtime +7 -exec btrfs subvolume delete -R "{}" +
 umount "$TMP"
 rmdir "$TMP"
