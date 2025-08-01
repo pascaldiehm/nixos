c0665c5f5d92c22ffcf956d5fca7337cb6e8a2b8
Pascal Diehm <pdiehm8@gmail.com>
2025-08-01 09:55:58 +0200

    Skip oil confirmation on simple edits

--- base/desktop/nvim.nix
+++ base/desktop/nvim.nix
@@ -272,6 +272,7 @@
           enable = true;
 
           settings = {
+            skip_confirm_for_simple_edits = true;
             use_default_keymaps = false;
             view_options.show_hidden = true;
 
