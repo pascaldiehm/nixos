6c19416806f2d7a97abe0228b66f15ea72a470fe
Pascal Diehm <pdiehm8@gmail.com>
2025-08-06 13:17:30 +0200

    Added networkmanager-openconnect plugin for university VPN

--- extra/university/networking.nix
+++ extra/university/networking.nix
@@ -1,67 +1,71 @@
-{ config, ... }: {
-  networking.networkmanager.ensureProfiles.profiles = {
-    bayern-wlan = {
-      connection = {
-        autoconnect-priority = 25;
-        id = "@BayernWLAN";
-        type = "wifi";
+{ config, pkgs, ... }: {
+  networking.networkmanager = {
+    plugins = [ pkgs.networkmanager-openconnect ];
+
+    ensureProfiles.profiles = {
+      bayern-wlan = {
+        connection = {
+          autoconnect-priority = 25;
+          id = "@BayernWLAN";
+          type = "wifi";
+        };
+
+        wifi = {
+          mode = "infrastructure";
+          ssid = "@BayernWLAN";
+        };
       };
 
-      wifi = {
-        mode = "infrastructure";
-        ssid = "@BayernWLAN";
-      };
-    };
+      eduroam = {
+        wifi-security.key-mgmt = "wpa-eap";
 
-    eduroam = {
-      wifi-security.key-mgmt = "wpa-eap";
+        "802-1x" = {
+          ca-cert = config.sops.secrets."university/eduroam/ca-cert".path;
+          client-cert = config.sops.secrets."university/eduroam/client-cert".path;
+          eap = "tls";
+          identity = "$EDUROAM_IDENTITY";
+          private-key = config.sops.secrets."university/eduroam/private-key".path;
+          private-key-password = "$EDUROAM_PASSWORD";
+        };
 
-      "802-1x" = {
-        ca-cert = config.sops.secrets."university/eduroam/ca-cert".path;
-        client-cert = config.sops.secrets."university/eduroam/client-cert".path;
-        eap = "tls";
-        identity = "$EDUROAM_IDENTITY";
-        private-key = config.sops.secrets."university/eduroam/private-key".path;
-        private-key-password = "$EDUROAM_PASSWORD";
-      };
+        connection = {
+          autoconnect-priority = 50;
+          id = "eduroam";
+          type = "wifi";
+        };
 
-      connection = {
-        autoconnect-priority = 50;
-        id = "eduroam";
-        type = "wifi";
-      };
-
-      wifi = {
-        mode = "infrastructure";
-        ssid = "eduroam";
-      };
-    };
-
-    university = {
-      connection = {
-        autoconnect = false;
-        id = "university";
-        type = "vpn";
+        wifi = {
+          mode = "infrastructure";
+          ssid = "eduroam";
+        };
       };
 
-      vpn = {
-        authtype = "password";
-        autoconnect-flags = 0;
-        certsigs-flags = 0;
-        cookie-flags = 2;
-        disable_udp = "no";
-        enable_csd_trojan = "no";
-        gateway = "vpngw.uni-wuerzburg.de";
-        gateway-flags = 2;
-        gwcert-flags = 2;
-        lasthost-flags = 0;
-        pem_passphrase_fsid = "no";
-        prevent_invalid_cert = "no";
-        protocol = "anyconnect";
-        resolve-flags = 2;
-        service-type = "org.freedesktop.NetworkManager.openconnect";
-        stoken_source = "disabled";
-        xmlconfig-flags = 0;
+      university = {
+        connection = {
+          autoconnect = false;
+          id = "university";
+          type = "vpn";
+        };
+
+        vpn = {
+          authtype = "password";
+          autoconnect-flags = 0;
+          certsigs-flags = 0;
+          cookie-flags = 2;
+          disable_udp = "no";
+          enable_csd_trojan = "no";
+          gateway = "vpngw.uni-wuerzburg.de";
+          gateway-flags = 2;
+          gwcert-flags = 2;
+          lasthost-flags = 0;
+          pem_passphrase_fsid = "no";
+          prevent_invalid_cert = "no";
+          protocol = "anyconnect";
+          resolve-flags = 2;
+          service-type = "org.freedesktop.NetworkManager.openconnect";
+          stoken_source = "disabled";
+          xmlconfig-flags = 0;
+        };
       };
     };
   };
