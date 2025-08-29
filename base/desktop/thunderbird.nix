a295a57fc6b27bfcfa75015615416ac8ee1174bb
Pascal Diehm <pdiehm8@gmail.com>
2025-08-29 17:10:28 +0200

    Added contacts and calendar to thunderbird

--- base/desktop/thunderbird.nix
+++ base/desktop/thunderbird.nix
@@ -2,13 +2,36 @@
   environment.persistence."/perm".users.pascal.directories = [ ".thunderbird/default" ];
 
   home-manager.users.pascal = {
-    accounts.email.accounts.default = {
-      address = "pdiehm8@gmail.com";
-      flavor = "gmail.com";
-      primary = true;
-      realName = "Pascal Diehm";
-      thunderbird.enable = true;
-      userName = "pdiehm8@gmail.com";
+    accounts = {
+      calendar.accounts.default = {
+        primary = true;
+        thunderbird.enable = true;
+
+        remote = {
+          type = "caldav";
+          url = "https://apidata.googleusercontent.com/caldav/v2/pdiehm8%40gmail.com/events/";
+          userName = "pdiehm8@gmail.com";
+        };
+      };
+
+      contact.accounts.default = {
+        thunderbird.enable = true;
+
+        remote = {
+          type = "carddav";
+          url = "https://www.googleapis.com/carddav/v1/principals/pdiehm8@gmail.com/lists/default/";
+          userName = "pdiehm8@gmail.com";
+        };
+      };
+
+      email.accounts.default = {
+        address = "pdiehm8@gmail.com";
+        flavor = "gmail.com";
+        primary = true;
+        realName = "Pascal Diehm";
+        thunderbird.enable = true;
+        userName = "pdiehm8@gmail.com";
+      };
     };
 
     programs.thunderbird = {
