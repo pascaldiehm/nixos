2d7187c853a0c397ca2cb8aa015bf54f4b57248e
Pascal Diehm <pdiehm8@gmail.com>
2025-09-17 20:48:51 +0200

    Embedded known host keys into configuration

--- base/common/hosts.nix
+++ base/common/hosts.nix
@@ -1,6 +1,4 @@
 {
-  programs.ssh.knownHostsFiles = [ ../../resources/known_hosts ];
-
   networking.hosts = {
     "192.168.1.88" = [ "bowser" ];
     "192.168.1.89" = [ "homeassistant" ];
@@ -8,5 +6,13 @@
     "192.168.1.91" = [ "pascal-laptop" ];
     "192.168.1.99" = [ "pascal-phone" ];
     "91.99.52.233" = [ "goomba" ];
+  };
+
+  programs.ssh.knownHosts = {
+    "[bowser]:1970".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILpY9HXNKIfGvwQTQ1rLidyoheecUiYaBFu6R2LSxiiG";
+    "[goomba]:1970".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFyFZp8gdn4egNOAtBA/U+eCxdHEyPU/fnLvwMqOtoFR";
+    "[pascal-laptop]:1970".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdTOVCs6qsCHb3TLXq0rGc7WUgUHgmjGxRxd4rHU+q1";
+    "[pascal-pc]:1970".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKSmgsw6WYhO5L9HvG4nhsW+WpTAYsR5UwwOQcmte4QU";
+    "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
   };
 }
--- extra/university/git.nix
+++ extra/university/git.nix
@@ -7,8 +7,12 @@
       "git@gitlab.informatik.uni-wuerzburg.de:s457719/".insteadOf = "uni:/";
     };
 
-    ssh.matchBlocks."gitlab.informatik.uni-wuerzburg.de".identityFile = ''
-      ${config.sops.secrets."university/gitlab-ssh-key".path}
-    '';
+    ssh.matchBlocks."gitlab.informatik.uni-wuerzburg.de" = {
+      identityFile = config.sops.secrets."university/gitlab-ssh-key".path;
+    };
+  };
+
+  programs.ssh.knownHosts."gitlab.informatik.uni-wuerzburg.de" = {
+    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHuymoMSN41qVyA1d1+awluFWRCElDIRa+ygAugtr9R";
   };
 }
