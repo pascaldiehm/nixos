f964b7e3784916f88f144b507bc41e77bef91d07
Pascal Diehm <pdiehm8@gmail.com>
2025-09-22 22:00:57 +0200

    Synced l alias behaviour between desktop and server

--- resources/zsh/aliases.zsh
+++ resources/zsh/aliases.zsh
@@ -1,14 +1,15 @@
 #!/usr/bin/env zsh
 
-alias l="ls -al"
 alias lsblk="lsblk -o NAME,TYPE,SIZE,PARTLABEL,LABEL,FSTYPE,MOUNTPOINTS"
 alias which="which -as"
 
 if [ "$NIXOS_MACHINE_TYPE" = "desktop" ]; then
+  alias l="ls -alg"
   alias ls="eza"
   alias open="xdg-open"
   alias py="python3"
 elif [ "$NIXOS_MACHINE_TYPE" = "server" ]; then
   alias grep="grep --color=auto"
+  alias l="ls -alh"
   alias ls="ls --color=auto"
 fi
