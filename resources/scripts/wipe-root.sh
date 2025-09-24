57a9f829c144b794b42da67098837e3842b8b5e2
Pascal Diehm <pdiehm8@gmail.com>
2025-09-24 08:17:35 +0200

    Fixed pre-boot command arguments

--- resources/scripts/wipe-root.sh
+++ resources/scripts/wipe-root.sh
@@ -6,7 +6,7 @@ mount /dev/disk/by-label/nixos "$TMP"
 mkdir -p "$TMP/history"
 find "$TMP/history" -mindepth 1 -maxdepth 1 -mtime +7 -exec btrfs subvolume delete --recursive "{}" +
 
-test -d "$TMP/root" && mv "$TMP/root" "$TMP/history/$(date --date "@$(stat --format "%Y" "$TMP/root")" "+%Y-%m-%d_%H:%M:%S")"
+test -d "$TMP/root" && mv "$TMP/root" "$TMP/history/$(date -d "@$(stat -c "%Y" "$TMP/root")" "+%Y-%m-%d_%H:%M:%S")"
 btrfs subvolume create "$TMP/root"
 
 umount "$TMP"
