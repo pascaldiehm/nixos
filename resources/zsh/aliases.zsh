0a78b30f7c3f21bf1c3dc662f0797fbab757ba4c
Pascal Diehm <pdiehm8@gmail.com>
2025-09-28 16:30:17 +0200

    Revert "Removed hidden flag from rg and fd"
    This reverts commit 7d817264259feedf33e5d0074beb5fd476e4a439.

--- resources/zsh/aliases.zsh
+++ resources/zsh/aliases.zsh
@@ -4,11 +4,12 @@ alias lsblk="lsblk --output NAME,TYPE,SIZE,PARTLABEL,LABEL,FSTYPE,MOUNTPOINTS"
 alias which="which -as"
 
 if [ "$NIXOS_MACHINE_TYPE" = "desktop" ]; then
+  alias fd="fd --hidden"
   alias l="ls --all --long --group"
   alias ls="eza"
   alias open="xdg-open"
   alias py="python3"
-  alias rg="rg --smart-case"
+  alias rg="rg --hidden --smart-case"
 elif [ "$NIXOS_MACHINE_TYPE" = "server" ]; then
   alias grep="grep --color=auto"
   alias l="ls -alh"
