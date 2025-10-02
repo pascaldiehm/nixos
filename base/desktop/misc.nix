92125ef521b927f593eeeab560f4ba238adcda00
Pascal Diehm <pdiehm8@gmail.com>
2025-10-02 11:11:35 +0200

    Remove docker containers when daemon stops on desktops

--- base/desktop/misc.nix
+++ base/desktop/misc.nix
@@ -1,4 +1,4 @@
-{
+{ lib, pkgs, ... }: {
   home-manager.users.pascal.programs.git.signing = {
     key = "E85EB0566C779A2F";
     signByDefault = true;
@@ -8,4 +8,8 @@
     substituters = [ "http://bowser:5778" "http://bowser:5779" ];
     trusted-public-keys = [ "private-1:Rx2/kvQOl7bTeQLc9hq8jV+7mJZaPZv3see8QbcXSmI=" ];
   };
+
+  systemd.services.docker.preStop = ''
+    ${lib.getExe pkgs.docker} container ls --all --quiet | xargs --no-run-if-empty ${lib.getExe pkgs.docker} container rm --force
+  '';
 }
