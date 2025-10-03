47ce94735b3d75b58e9bdacf4371778d3e10331f
Pascal Diehm <pdiehm8@gmail.com>
2025-10-03 12:08:24 +0200

    Installed nix-serve on bowser

--- machines/bowser/misc.nix
+++ machines/bowser/misc.nix
@@ -1,1 +1,6 @@
-{ environment.persistence."/perm".users.pascal.directories = [ "shared" ]; }
+{ pkgs, ... }: {
+  environment = {
+    persistence."/perm".users.pascal.directories = [ "shared" ];
+    systemPackages = [ pkgs.nix-serve ];
+  };
+}
