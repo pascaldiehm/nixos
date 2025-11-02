c64b29ee2326cc34be7c5247f4c9a798aa96ce30
Pascal Diehm <pdiehm8@gmail.com>
2025-11-02 15:19:12 +0100

    Use networking.extraHosts for hostname config

--- base/common/hosts.nix
+++ base/common/hosts.nix
@@ -1,12 +1,12 @@
 {
-  networking.hosts = {
-    "192.168.1.88" = [ "bowser" ];
-    "192.168.1.89" = [ "homeassistant" ];
-    "192.168.1.90" = [ "pascal-pc" ];
-    "192.168.1.91" = [ "pascal-laptop" ];
-    "192.168.1.99" = [ "pascal-phone" ];
-    "91.99.52.233" = [ "goomba" ];
-  };
+  networking.extraHosts = ''
+    192.168.1.88 bowser
+    192.168.1.89 homeassistant
+    192.168.1.90 pascal-pc
+    192.168.1.91 pascal-laptop
+    192.168.1.99 pascal-phone
+    91.99.52.233 goomba
+  '';
 
   programs.ssh.knownHosts = {
     "[bowser]:1970".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILpY9HXNKIfGvwQTQ1rLidyoheecUiYaBFu6R2LSxiiG";
