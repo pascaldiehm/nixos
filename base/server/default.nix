5ed4eacc327d9bc548b47c061b5b1708954f66ff
Pascal Diehm <pdiehm8@gmail.com>
2025-11-04 11:11:45 +0100

    Removed server fallback network

--- base/server/default.nix
+++ base/server/default.nix
@@ -1,1 +1,1 @@
-{ imports = [ ./docker.nix ./journalwatch.nix ./misc.nix ./networking.nix ./upgrade.nix ]; }
+{ imports = [ ./docker.nix ./journalwatch.nix ./misc.nix ./upgrade.nix ]; }
--- base/server/misc.nix
+++ base/server/misc.nix
@@ -1,6 +1,8 @@
 {
   environment.persistence."/perm".users.pascal.directories = [ "docker" ];
+  networking.usePredictableInterfaceNames = false;
   security.sudo.wheelNeedsPassword = false;
+  systemd.network.enable = true;
 
   services.fail2ban = {
     enable = true;
