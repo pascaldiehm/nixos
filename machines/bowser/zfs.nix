01c4a63a372676c291ad78b2cbd8cf990b19f71a
Pascal Diehm <pdiehm8@gmail.com>
2025-11-07 21:11:36 +0100

    Removed redundant ZFS support declaration for bowser

--- machines/bowser/zfs.nix
+++ machines/bowser/zfs.nix
@@ -1,11 +1,7 @@
 {
+  boot.zfs.forceImportRoot = false;
   networking.hostId = "06378c60";
   services.zfs.autoScrub.enable = true;
-
-  boot = {
-    supportedFilesystems.zfs = true;
-    zfs.forceImportRoot = false;
-  };
 
   fileSystems."/home/pascal/archive" = {
     device = "archive";
