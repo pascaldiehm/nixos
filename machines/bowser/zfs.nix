de04ed0b316d371dab567810bd8a41e83fb89a59
Pascal Diehm <pdiehm8@gmail.com>
2025-11-09 22:38:00 +0100

    Added ZFS report via NTFY

--- machines/bowser/zfs.nix
+++ machines/bowser/zfs.nix
@@ -1,11 +1,20 @@
-{
+{ config, ... }: {
   boot.zfs.forceImportRoot = false;
   networking.hostId = "06378c60";
-  services.zfs.autoScrub.enable = true;
 
   fileSystems."/home/pascal/archive" = {
     device = "archive";
     fsType = "zfs";
     options = [ "nofail" "x-systemd.device-timeout=10" ];
+  };
+
+  services.zfs = {
+    autoScrub.enable = true;
+
+    zed.settings = {
+      ZED_NTFY_ACCESS_TOKEN = "$(cat \"${config.sops.common.ntfy.path}\")";
+      ZED_NTFY_TOPIC = "bowser-zfs";
+      ZED_NTFY_URL = "https://ntfy.pdiehm.dev";
+    };
   };
 }
