e29038af55c57e49d1f77ce13f102e91bbf5ab5e
Pascal Diehm <pdiehm8@gmail.com>
2025-11-10 09:18:07 +0100

    Send non-critical notifications for ZFS events

--- machines/bowser/zfs.nix
+++ machines/bowser/zfs.nix
@@ -12,6 +12,7 @@
     autoScrub.enable = true;
 
     zed.settings = {
+      ZED_NOTIFY_VERBOSE = true;
       ZED_NTFY_ACCESS_TOKEN = "$(cat \"${config.sops.common.ntfy.path}\")";
       ZED_NTFY_TOPIC = "bowser-zfs";
       ZED_NTFY_URL = "https://ntfy.pdiehm.dev";
