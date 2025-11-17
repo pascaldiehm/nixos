0fe95e6b1d5504d153c5535a83c8de0f6563a65a
Pascal Diehm <pdiehm8@gmail.com>
2025-11-17 08:54:36 +0100

    Always reboot server after upgrade

--- base/server/upgrade.nix
+++ base/server/upgrade.nix
@@ -1,10 +1,12 @@
-{
+{ lib, pkgs, ... }: {
+  systemd.services.nixos-upgrade.serviceConfig.ExecStartPost = "${lib.getExe' pkgs.systemd "systemctl"} reboot";
+
   system.autoUpgrade = {
     enable = true;
-    allowReboot = true;
     dates = "Mon 01:00";
     flags = [ "--impure" ];
     flake = "github:pascaldiehm/nixos";
+    operation = "boot";
     upgrade = false;
   };
 }
