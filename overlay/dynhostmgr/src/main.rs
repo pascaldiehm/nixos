2c58d880bc5006a1822b7a6c954040c5ae5716ba
Pascal Diehm <pdiehm8@gmail.com>
2025-12-10 22:08:22 +0100

    feat(dynhostmgr): ping hosts asynchronously

--- overlay/dynhostmgr/src/main.rs
+++ overlay/dynhostmgr/src/main.rs
@@ -2,9 +2,11 @@ use std::{
     collections::BTreeMap,
     fs, io, mem,
     net::{AddrParseError, IpAddr, Ipv6Addr},
-    thread,
     time::{Duration, SystemTime},
 };
+
+use tokio::{task::JoinHandle, time};
+use tokio_icmp_echo::Pinger;
 
 const FILE_HOSTS: &str = "/etc/hosts";
 const FILE_HOSTS_NEW: &str = "/etc/.hosts.new";
@@ -16,11 +18,13 @@ const TIMEOUT: Duration = Duration::from_millis(250);
 const INTERVAL: Duration = Duration::from_millis(1000);
 
 type Hosts = BTreeMap<String, Host>;
+type Ping = Result<Option<Duration>, tokio_icmp_echo::Error>;
 
 #[derive(Default)]
 struct Host {
     addr: Option<IpAddr>,
     addrs: Vec<IpAddr>,
+    pings: Vec<(IpAddr, JoinHandle<Ping>)>,
 }
 
 macro_rules! pushln {
@@ -36,10 +40,10 @@ fn load_config(stamp: &mut SystemTime) -> io::Result<Option<String>> {
     if time > *stamp {
         println!("Loading configuration...");
         *stamp = time;
-        return Ok(Some(fs::read_to_string(FILE_DYN_HOSTS)?));
+        Ok(Some(fs::read_to_string(FILE_DYN_HOSTS)?))
+    } else {
+        Ok(None)
     }
-
-    Ok(None)
 }
 
 fn parse_config(config: &str) -> Result<Hosts, AddrParseError> {
@@ -59,14 +63,6 @@ fn parse_config(config: &str) -> Result<Hosts, AddrParseError> {
 
     println!("Done. Managing {} hosts.", hosts.len());
     Ok(hosts)
-}
-
-fn ping(addr: &&IpAddr) -> bool {
-    ping::new(**addr)
-        .timeout(TIMEOUT)
-        .socket_type(ping::RAW) // TODO: Remove this once DGRAM sockets work
-        .send()
-        .is_ok()
 }
 
 fn update_hosts(data: &str) -> io::Result<()> {
@@ -106,36 +102,53 @@ fn update_hosts(data: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut stamp = SystemTime::UNIX_EPOCH;
     let mut hosts = Hosts::new();
+    let pinger = Pinger::new().await.expect("Failed to create pinger");
+    let mut seq = 0u16;
 
     loop {
         match load_config(&mut stamp) {
-            Err(err) => eprintln!("Failed to read {FILE_DYN_HOSTS}: {err}"),
             Ok(None) => (),
+            Err(err) => eprintln!("Failed to read {FILE_DYN_HOSTS}: {err}"),
 
             Ok(Some(config)) => match parse_config(&config) {
-                Err(err) => eprintln!("Failed to parse config: {err}"),
                 Ok(cfg) => hosts = cfg,
+                Err(err) => eprintln!("Failed to parse config: {err}"),
             },
+        }
+
+        for host in hosts.values_mut() {
+            for addr in &host.addrs {
+                let ping = pinger.ping(*addr, 0x5044, seq, TIMEOUT);
+                host.pings.push((*addr, tokio::spawn(ping)));
+            }
         }
 
         let mut addrs = BTreeMap::new();
         addrs.insert(ADDR_VOID, vec!["void"]);
 
         for (name, host) in &mut hosts {
-            let addr = host.addrs.iter().find(ping).copied();
-            let prev = mem::replace(&mut host.addr, addr);
+            let pings = mem::take(&mut host.pings);
+            let prev = host.addr.take();
 
-            match (prev, addr) {
+            for (addr, ping) in pings {
+                if matches!(ping.await, Ok(Ok(Some(_)))) {
+                    host.addr = Some(addr);
+                    break;
+                }
+            }
+
+            match (prev, host.addr) {
                 (None, Some(addr)) => println!("{name}: {addr}"),
                 (Some(_), None) => println!("{name}: N/A"),
                 (Some(from), Some(to)) if from != to => println!("{name}: {from} -> {to}"),
                 _ => (),
             }
 
-            let addr = addr.unwrap_or(ADDR_VOID);
+            let addr = host.addr.unwrap_or(ADDR_VOID);
             addrs.entry(addr).or_default().push(name);
         }
 
@@ -158,6 +171,7 @@ fn main() {
             eprintln!("Failed to update {FILE_HOSTS}: {err}");
         }
 
-        thread::sleep(INTERVAL);
+        time::sleep(INTERVAL).await;
+        seq = seq.wrapping_add(1);
     }
 }
