c70e6cfbbeff17c77526649dcd997570a8aa9b91
Pascal Diehm <pdiehm8@gmail.com>
2025-12-27 13:10:53 +0100

    fix(server): automatic upgrade without nx

--- base/server/upgrade.nix
+++ base/server/upgrade.nix
@@ -1,15 +1,16 @@
-{ lib, pkgs, ... }: {
+{ pkgs, ... }: {
   systemd.services.nixos-upgrade = {
     after = [ "network-online.target" ];
     description = "Upgrade NixOS";
     path = [ pkgs.git pkgs.nixos-rebuild pkgs.sudo ];
     requires = [ "network-online.target" ];
+    serviceConfig.Type = "oneshot";
     startAt = "Mon 01:00";
 
-    serviceConfig = {
-      ExecStart = "${lib.getExe pkgs.scripts.nx} upgrade";
-      ExecStartPost = "${lib.getExe' pkgs.systemd "systemctl"} reboot";
-      Type = "oneshot";
-    };
+    script = ''
+      sudo -u pascal git -C /home/pascal/.config/nixos pull
+      nixos-rebuild --impure --flake /home/pascal/.config/nixos boot
+      systemctl reboot
+    '';
   };
 }
