db6b481511c7e0ef51bda0825a0bb4341fe81631
Pascal Diehm <pdiehm8@gmail.com>
2025-12-30 10:54:14 +0100

    ci(verify): remove timeout

--- apps/verify.sh
+++ apps/verify.sh
@@ -8,7 +8,7 @@ echo "## Systems" >>"$GITHUB_STEP_SUMMARY"
 rm -f /nix/var/nix/gcroots/ci/*
 
 while read -r MACHINE; do
-  if timeout 1h nix build -o "/nix/var/nix/gcroots/ci/$MACHINE" ".#nixosConfigurations.$MACHINE.config.system.build.toplevel" |& sed "s/^evaluation warning: /::warning::/"; then
+  if nix build -o "/nix/var/nix/gcroots/ci/$MACHINE" ".#nixosConfigurations.$MACHINE.config.system.build.toplevel" |& sed "s/^evaluation warning: /::warning::/"; then
     echo "- :white_check_mark: $MACHINE" >>"$GITHUB_STEP_SUMMARY"
   else
     echo "- :x: $MACHINE" >>"$GITHUB_STEP_SUMMARY"
