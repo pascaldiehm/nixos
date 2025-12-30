e8fbc540d0c05d47f666fcc7e63c3000550bfa74
Pascal Diehm <pdiehm8@gmail.com>
2025-12-30 10:54:26 +0100

    feat(installer): use cache if available

--- extra/installer.nix
+++ extra/installer.nix
@@ -2,8 +2,13 @@
   imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ];
   console.keyMap = "de";
   environment.systemPackages = [ (pkgs.writeShellScriptBin "nixinstall" "nix run github:pascaldiehm/nixos#install") ];
-  nix.settings.experimental-features = [ "flakes" "nix-command" "pipe-operators" ];
   nixpkgs.hostPlatform = "x86_64-linux";
+
+  nix.settings = {
+    experimental-features = [ "flakes" "nix-command" "pipe-operators" ];
+    substituters = lib.mkForce [ "http://127.0.0.1:5777" ];
+    trusted-public-keys = [ "private:Gj04okCf2KAYVNSQ5vwXCgOLRz+ESUGi/YlvT1rsnpQ=" ];
+  };
 
   services = {
     getty.helpLine = lib.mkAfter "Run 'nixinstall' to install NixOS.";
@@ -15,6 +20,39 @@
       publish = {
         enable = true;
         addresses = true;
+      };
+    };
+
+    nginx = {
+      enable = true;
+
+      virtualHosts.nix-substituter-proxy = {
+        locations = {
+          "/" = {
+            proxyPass = "http://192.168.1.88:5779";
+
+            extraConfig = ''
+              error_page 404 502 504 = @fallback;
+              proxy_connect_timeout 100ms;
+              proxy_intercept_errors on;
+            '';
+          };
+
+          "@fallback" = {
+            proxyPass = "https://cache.nixos.org";
+
+            extraConfig = ''
+              proxy_ssl_server_name on;
+              proxy_ssl_trusted_certificate /etc/ssl/certs/ca-certificates.crt;
+              proxy_ssl_verify on;
+            '';
+          };
+        };
+
+        listen = lib.singleton {
+          addr = "127.0.0.1";
+          port = 5777;
+        };
       };
     };
   };
