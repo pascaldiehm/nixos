d7651c132137ee02e06c00f363df1fac525e1bfa
Pascal Diehm <pdiehm8@gmail.com>
2026-01-05 15:27:06 +0100

    feat(desktop): add UWSM wrapper script

--- base/desktop/boot.nix
+++ base/desktop/boot.nix
@@ -1,4 +1,7 @@
 { lib, pkgs, ... }: {
+  home-manager.users.pascal.home.packages = [ pkgs.pd.hypr ];
+  programs.scripts.hypr.text = "exec ${lib.getExe pkgs.uwsm} start -Fe -D Hyprland -N Hyprland -g -1 /run/current-system/sw/bin/start-hyprland";
+
   boot = {
     initrd.luks.devices.nixos.device = "/dev/disk/by-partlabel/nixos";
     kernelPackages = pkgs.linuxPackages_latest;
@@ -12,7 +15,7 @@
       default_session.command = "${lib.getExe' pkgs.greetd "agreety"} --cmd ${pkgs.writeShellScript "exec-shell" ''exec "$SHELL"''}";
 
       initial_session = {
-        command = "${lib.getExe pkgs.uwsm} start -F -g -1 /run/current-system/sw/bin/Hyprland";
+        command = lib.getExe pkgs.pd.hypr;
         user = "pascal";
       };
     };
