e0e94b4061e18b02f7d4c1f2562ca2629428a52d
Pascal Diehm <pdiehm8@gmail.com>
2026-01-22 21:51:40 +0100

    refactor(godot-editor): use CR instead of Return

--- resources/scripts/godot-editor.sh
+++ resources/scripts/godot-editor.sh
@@ -6,7 +6,7 @@ if [ "$#" != 3 ]; then
   echo "Usage: godot-editor <project> <file> <line>"
   exit 1
 elif nc -z 127.0.0.1 6004; then
-  nvim --server 127.0.0.1:6004 --remote-send "<Cmd>e +$3 $2<Return>"
+  nvim --server 127.0.0.1:6004 --remote-send "<Cmd>e +$3 $2<CR>"
 else
   cd "$1"
   exec kitty nvim --listen 127.0.0.1:6004 "+$3" "$2"
